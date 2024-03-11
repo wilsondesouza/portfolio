637d2cb9b494125700b39e5059a29a5544be9975
wilsondesouza <wilson.de.souza.junior@gmail.com>
2024-03-10 23:48:23 -0300

    Adicionando carousel

--- main.js
+++ main.js
@@ -56,6 +56,7 @@ const projectsParagraph = document.getElementById('projects-paragraph');
 const pythonDetails = document.getElementById('python-details');
 const reactDetails = document.getElementById('react-details');
 const flutterDetails = document.getElementById('flutter-details');
+const othersProjects = document.getElementById('others-projects');
 const unityDetails = document.getElementById('unity-details');
 const javascriptDetails = document.getElementById('javascript-details');
 const chatDetails = document.getElementById('chat-details');
@@ -73,7 +74,7 @@ const flag = document.getElementById('flag');
 
 // Guardar texto original (Em português)
 // Save original text (In Portuguese)
-const conteudoOriginal = [inicioNav.textContent, sobreNav.textContent, projetosNav.textContent, contatoNav.textContent, curriculum.href, hello.textContent, aboutParagraph.innerHTML, freelance.textContent, education.textContent, certificates.textContent, engenharia.textContent, logistica.textContent, devJava.textContent, devFlutter.textContent, projectsTitle.textContent, projectsParagraph.innerHTML, pythonDetails.textContent, reactDetails.textContent, flutterDetails.textContent, unityDetails.textContent, javascriptDetails.textContent, chatDetails.textContent, maisProjetos.textContent, contactParagraph.textContent, developer.textContent, host.textContent,];
+const conteudoOriginal = [inicioNav.textContent, sobreNav.textContent, projetosNav.textContent, contatoNav.textContent, curriculum.href, hello.textContent, aboutParagraph.innerHTML, freelance.textContent, education.textContent, certificates.textContent, engenharia.textContent, logistica.textContent, devJava.textContent, devFlutter.textContent, projectsTitle.textContent, projectsParagraph.innerHTML, pythonDetails.textContent, reactDetails.textContent, flutterDetails.textContent, othersProjects.textContent, unityDetails.textContent, javascriptDetails.textContent, chatDetails.textContent, maisProjetos.textContent, contactParagraph.textContent, developer.textContent, host.textContent,];
 
 // Alterar conteúdo dos textos parar inglês
 // Change texts content to English
@@ -86,7 +87,7 @@ function alterarConteudo() {
     sobreNav.textContent = 'About';
     projetosNav.textContent = 'Projects';
     contatoNav.textContent = 'Contact';
-    curriculum.href = '../assets/curriculum/wilsonsouzajunior_english.pdf';
+    curriculum.href = 'assets/curriculum/wilsonsouzajunior_english.pdf';
     hello.textContent = 'Hello, my name is Wilson';
     aboutParagraph.innerHTML = 'Briefly, I can say that I consider myself an organized and systematic person, ethical and professional, investigative, conventional and realistic (References to the Holland test).<br><br> Because of this, I decided to make a career transition to one that better suits my profile, and so I decided to enter the IT area when I started my studies in Computer Engineering. <br><br> Since then, I have constantly sought new knowledge about the various technologies in the area, always qualifying myself through certificates. And to put the acquired skills into practice, I seek an internship, to also learn new skills and tools with the intention of developing my career.';
     freelance.textContent = 'Working on my own personal projects and for small entrepreneur in the region';
@@ -97,10 +98,11 @@ function alterarConteudo() {
     devJava.textContent = 'Java development with Cloud';
     devFlutter.textContent = 'Mobile Development with Flutter';
     projectsTitle.textContent = 'Projects';
-    projectsParagraph.innerHTML = 'Below I will leave some projects that I have already developed. From simple, practical, functional projects to the most complex. <br><br> Some projects were created to serve as an assessment for some courses, and others were made to assimilate and solidify my knowledge and skills, these being personal projects.';
+    projectsParagraph.innerHTML = 'Below I will leave some projects that I have already developed. From simple, practical, functional projects to the most complex. <br> Some projects were created to serve as an assessment for some courses, and others were made to assimilate and solidify my knowledge and skills, these being personal projects.';
     pythonDetails.textContent = 'Desktop application programmed in Python using the Custom Tkinter library for the creation of the graphical interface. Dictionaries were used to search for and return values and lists for the generation of buttons.';
     reactDetails.textContent = 'Website built to complete the Nu Institute course. Used React library to make the site responsive and interactive. Vanilla JavaScript was also used to apply some logic to the Shopping Cart and Theme change.';
     flutterDetails.textContent = 'Flutter app for Android. Initially built to meet the needs of a local entrepreneur, and later adapted for personal use. It uses some dependencies such as local-auth for biometrics, shared-preferences for data persistence, and provider for reusing widgets.';
+    othersProjects.textContent = 'Others Projects';
     unityDetails.textContent = "Game developed in Unity with C#. It's a simple 3D shooter game with a character who can jump, move, pick up items, and destroy enemies. The game has a simple menu and a 'game over' screen";
     javascriptDetails.textContent = 'Webgame with simple HTML construction, some CSS stylings and JavaScript programming to interact with the user.';
     chatDetails.textContent = 'Application using React to consume the Open Ai API to simulate a generative AI of texts.';
@@ -136,19 +138,20 @@ function restaurarConteudo() {
     pythonDetails.textContent = conteudoOriginal[16];
     reactDetails.textContent = conteudoOriginal[17];
     flutterDetails.textContent = conteudoOriginal[18];
-    unityDetails.textContent = conteudoOriginal[19];
-    javascriptDetails.textContent = conteudoOriginal[20];
-    chatDetails.textContent = conteudoOriginal[21];
-    maisProjetos.textContent = conteudoOriginal[22];
-    contactParagraph.textContent = conteudoOriginal[23];
-    developer.textContent = conteudoOriginal[24];
-    host.textContent = conteudoOriginal[25];
+    othersProjects.textContent = conteudoOriginal[19];
+    unityDetails.textContent = conteudoOriginal[20];
+    javascriptDetails.textContent = conteudoOriginal[21];
+    chatDetails.textContent = conteudoOriginal[22];
+    maisProjetos.textContent = conteudoOriginal[23];
+    contactParagraph.textContent = conteudoOriginal[24];
+    developer.textContent = conteudoOriginal[25];
+    host.textContent = conteudoOriginal[26];
 }
 
 // Botão Mudar
 botaoMudar.addEventListener('click', function () {
     if (inicioNav.textContent === "Início") {
-        flag.src = "../assets/images/brasil.png";
+        flag.src = "assets/images/brasil.png";
         alterarConteudo();
     } 
 });
@@ -156,7 +159,7 @@ botaoMudar.addEventListener('click', function () {
 // Botão Restaurar
 botaoRestaurar.addEventListener('click', function () {
     if (inicioNav.textContent !== "Início") {
-        flag.src = "../assets/images/estados-unidos.png";
+        flag.src = "assets/images/estados-unidos.png";
         restaurarConteudo();
     }
 });
@@ -164,10 +167,10 @@ botaoRestaurar.addEventListener('click', function () {
 // Botão Idioma
 botaoIdioma.addEventListener('click', function () {
     if (inicioNav.textContent === "Início") {
-        flag.src = "../assets/images/brasil.png";
+        flag.src = "assets/images/brasil.png";
         alterarConteudo();
     } else {
-        flag.src = "../assets/images/estados-unidos.png";
+        flag.src = "assets/images/estados-unidos.png";
         restaurarConteudo();
     }
 });
