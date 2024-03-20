1ad01f6983e0ace205ec526baacb29a52d7e2d7c
wilsondesouza <wilson.de.souza.junior@gmail.com>
2024-03-20 17:47:46 -0300

    Empty-Commit

--- js/theme.js
+++ js/theme.js
@@ -16,6 +16,8 @@ themeToggle.addEventListener('click', () => {
         document.documentElement.style.setProperty('--colorButton-dark-hover', '#24262a');
         document.documentElement.style.setProperty('--split-background-white', '#2e3033');
         document.documentElement.style.setProperty('--theme-toggle-white', 'gold');
+        document.documentElement.style.setProperty('--navBarColor-dark', 'rgba(255, 255, 255, 0.9)');
+
 
         /*document.documentElement.style.setProperty('--background-gradient-escuro', 'linear-gradient(90deg, rgba(0,0,0,1) 10%, rgba(66,70,72,1) 30%, rgba(233,238,240,1) 50%, rgba(66,70,72,1) 70%, rgba(0,0,0,1) 90%)');*/
     } else {
@@ -29,6 +31,7 @@ themeToggle.addEventListener('click', () => {
         document.documentElement.style.setProperty('--colorButton-dark-hover', 'white');
         document.documentElement.style.setProperty('--split-background-white', '#f7f9fb');
         document.documentElement.style.setProperty('--theme-toggle-white', 'white');
+        document.documentElement.style.setProperty('--navBarColor-dark', 'rgba(36, 38, 42, 0.9)');
 
         /*document.documentElement.style.setProperty('--background-gradient-escuro', 'linear-gradient(90deg, #CECECE 10%, rgba(66,70,72,1) 30%, rgba(0,0,0,1) 50%, rgba(66,70,72,1) 70%, #CECECE) 90%');*/
     }
